951f5ac53ea940d1330e3292ee1952e1e907fefc
K-Musembi <kevinmusembi.m@gmail.com>
2024-12-30 16:42:54 +0300

    added an image upload component. user can now upload file or take a photo

--- frontend/src/components/ImageUpload.js
+++ frontend/src/components/ImageUpload.js
@@ -1,21 +1,39 @@
-import React, { useState } from "react";
+import React, { useState, useRef, useCallback } from "react";
 import { useNavigate } from "react-router-dom";
+import Webcam from "react-webcam";
 import { API_URL } from "../config";
 
 const ImageUpload = ({ onResult }) => {
-    const [file, setFile] = useState(null);
+    const [img, setImg] = useState(null);
+    const [showWebcam, setShowWebcam] = useState(true);
     const navigate = useNavigate();
+    const webcamRef = useRef(null);
 
     const handleFileChange = (event) => {
-        setFile(event.target.files[0]);  // when event occurs, set file
+        const file = event.target.files[0]
+        setImg(file);  // when event occurs, set file
+        setShowWebcam(false);
     };
 
-    const handleUpload = async () => {
-        if (!file) return;  // return if no file is selected
+    const captureImage = useCallback(() => {
+        const imageSrc = webcamRef.current.getScreenshot();
+        if (imageSrc) {
+            // convert base64 to file
+            fetch(imageSrc)
+                .then(res => res.blob())
+                .then(blob => {
+                    setImg(new File([blob], 'webcam.jpg', { type: 'image/jpeg' }));
+                    setShowWebcam(false);
+                });
+        }
+    }, [webcamRef]);
+
+    const handleUpload = async () => { 
+        if (!img) return;  // return if no file is selected
 
         const data = new FormData();
-        data.append("image", file);
-
+        data.append("image", img);
+        
         try {
             const response = await fetch(`${API_URL}/upload`, {
                 method: "POST",
@@ -29,14 +47,23 @@ const ImageUpload = ({ onResult }) => {
         } catch (err) {
             console.error("Upload failed:", err);
         }
-    };  
+    };
+
     return (
         <div className="image-upload-container">
             <h2>Upload a mango leaf image</h2>
-            <label htmlFor="file-upload" className="custom-file-upload">
+            {/*<label htmlFor="file-upload" className="custom-file-upload">
                 Choose File
-            </label>
-            <input id="file-upload" type="file" accept="image/*" style={{ display: 'none' }} onChange={handleFileChange} />
+            </label>*/}
+            <input id="file-upload" type="file" accept="image/*" onChange={handleFileChange} />
+            {showWebcam ? (
+                <Webcam ref={webcamRef} screenshotFormat="image/jpeg" />
+            ) : (
+                <div className="preview">
+                    <img src={URL.createObjectURL(img)} alt="preview" />
+                </div>
+            )}
+            <button onClick={captureImage}>Take leaf photo</button>
             <button onClick={handleUpload}>Upload</button>
         </div>
     );
